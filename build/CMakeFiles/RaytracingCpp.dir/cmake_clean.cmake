e4369c0d874c82cd9816f13e1bf72d3acb012691
UnmarkedBanshee <dxp38@pct.edu>
2023-12-16 23:45:39 -0500

    feature: configured makefiles

---
+++ build/CMakeFiles/RaytracingCpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RaytracingCpp.dir/test.cpp.o"
+  "CMakeFiles/RaytracingCpp.dir/test.cpp.o.d"
+  "RaytracingCpp"
+  "RaytracingCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RaytracingCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- test.cpp
+++ test.cpp
@@ -1,8 +1,36 @@
-//
-// Created by Dax Patel on 12/16/23.
-//
+#include <iostream>
 
+using namespace std;
 
 int main() {
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        for (int i = 0; i < image_width; ++i) {
+            //this is calculating the RGB values of the image
+            double r = double(i) / (image_width - 1);
+            double g = double(j) / (image_height - 1);
+            double b = 0;
+
+            /**
+             * Color Conversion to Integer Values: The red, green, and blue components
+             * are converted from a range of 0 to 1 to a
+             * range of 0 to 255 (typical for RGB color values in digital images).
+             * This is done by multiplying each component by 255.999 and then casting it to an integer.
+             */
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            cout << ir << ' ' << ig << ' ' << ib << "\n";
+        }
+    }
 
 }
